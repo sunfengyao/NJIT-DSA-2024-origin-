01aed32a9f410d3749e194e16a38d4dc1c9add35
sunfengyao <2501458412@qq.com>
2024-04-04 12:43:57 +0800

    change

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,79 +1,80 @@
 package oy.tol.tra;
 
+
+
 public class Algorithms {
-    public static <T>void swap(T[] array,int m,int n){
-        T tmp=array[m];
-        array[m]=array[n];
-        array[n]=tmp;
-    }
-    
-    public static <T extends Comparable<T>> void sort(T [] array){
-        int n = array.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if(array[j].compareTo(array[j+1])>0){
-                    swap(array, j, j+1);
-                }
-            }
-        }
-    }
-    
-    public static <T> void reverse(T [] array){
-        int head=0;
-        int tail=array.length-1;
-        while(head<tail){
-            swap(array,head,tail);
-            head++;
-            tail--;
+    public static <T> void reverse(T [] array) {
+        int i = 0;
+        while (i < array.length/2) {
+            swap(array,i,array.length-i-1);
+            i++;
         }
     }
 
-    public static <T extends Comparable<T>> int binarySearch(T aValue, T [] fromArray, int fromIndex, int toIndex) {
-        while(fromIndex<=toIndex){
-            int mid=fromIndex+(toIndex-fromIndex)/2;
-            if(aValue.compareTo(fromArray[mid])>0){
-                fromIndex=mid+1;
-            }else if(aValue.compareTo(fromArray[mid])<0){
-                toIndex=mid-1;
-            }else{
-                return mid;
+    
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        for (int i=0;i<array.length-1;i++){
+            for (int j=0;j<array.length-1-i;j++){
+                if (array[j].compareTo(array[j+1]) > 0) {
+                    swap(array,j,j+1);
+                }
+
             }
         }
-        return -1;
     }
 
     public static <E extends Comparable<E>> void fastSort(E [] array) {
-        quickSort(array, 0, array.length - 1);
-    }
-
-    public static <E extends Comparable<E>> void quickSort(E [] array, int begin, int end) {
-        if(begin<end){
-            int partitionIndex=partition(array, begin, end);
-            quickSort(array, begin, partitionIndex-1);
-            quickSort(array, partitionIndex+1, end);
-        }else{
+        if (array == null || array.length <= 1){
             return;
         }
-        
+        quickSort(array,0,array.length-1);
     }
+
+    
+    public static <E extends Comparable<E>> void quickSort(E [] array, int begin, int end){
+        if (begin < end){
+            int q = partition(array,begin,end);
+            quickSort(array,begin,q-1);
+            quickSort(array,q+1,end);
+        }
+    }
+
+    
     private static <E extends Comparable<E>> int partition(E [] array, int begin, int end) {
-        E p=array[begin];
-        int head = begin;
-        int tail = end;
-        while(head!=tail){
-            while ((head<tail)&&array[head].compareTo(p)<=0) {
-                head++;
-            }
-            while ((head<tail)&&array[tail].compareTo(p)>0) {
-                tail--;
-            }
-            if(head<tail){
-                swap(array, head, tail);
+        int i = begin-1;
+        for (int leftIndex = begin;leftIndex<end;leftIndex++){
+            if (array[leftIndex].compareTo(array[end])<0){
+                i++;
+                swap(array,i,leftIndex);
             }
         }
-        array[begin]=array[head];
-        array[head]=p;
-        return head;
+        swap(array,i+1,end);
+        return i+1;
+    }
+
+   
+    public static <T> void swap(T[] array,int i,int j){
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(T aValue, T [] fromArray, int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            return -1;
+        }
+
+        int middle = (fromIndex+toIndex)/2;
+
+        if (aValue.compareTo(fromArray[middle]) == 0) {
+            return middle;
+        }
+        else if (aValue.compareTo(fromArray[middle]) < 0) {
+            return binarySearch(aValue, fromArray, fromIndex, middle - 1);
+        }
+        else {
+            return binarySearch(aValue, fromArray, middle + 1, toIndex);
+        }
+
     }
 }
-
